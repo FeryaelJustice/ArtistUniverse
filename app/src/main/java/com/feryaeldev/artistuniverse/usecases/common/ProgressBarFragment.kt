4ac97db80896d9194cea1216416799482eb22bef
DESKTOP-519O059\nano9 <nano0gs@hotmail.es>
2021-07-26 00:17:21 +0200

    build.gradle fix and binding working, ProgressBarFragment.kt (only kotlin, not xml) done.

---
+++ app/src/main/java/com/feryaeldev/artistuniverse/usecases/common/ProgressBarFragment.kt
@@ -0,0 +1,32 @@
+package com.feryaeldev.artistuniverse.usecases.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.feryaeldev.artistuniverse.databinding.FragmentProgressBarBinding
+
+class ProgressBarFragment : Fragment() {
+
+    private var _binding: FragmentProgressBarBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProgressBarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ProgressBarFragment()
+    }
+}
